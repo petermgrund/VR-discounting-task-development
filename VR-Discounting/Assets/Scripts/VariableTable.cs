7c7372649d36e2a73d590d76ccc647be5f7e9eb2
Svenskbygderna <87161151+petermgrund@users.noreply.github.com>
2022-01-03 14:46:13 -0600

    moving table achieved!
    Co-Authored-By: Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>

--- VR-Discounting/Assets/Scripts/VariableTable.cs
+++ VR-Discounting/Assets/Scripts/VariableTable.cs
@@ -13,6 +13,7 @@ public class VariableTable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += Vector3.forward * Time.deltaTime;
+
     }
 }
