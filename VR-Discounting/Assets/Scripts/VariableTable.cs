85a842004cb8bb2eb1ec4130cee6aa164fa9df0c
Svenskbygderna <87161151+petermgrund@users.noreply.github.com>
2022-01-03 17:41:34 -0600

    got if statement to work (again)
    Co-Authored-By: Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>

--- VR-Discounting/Assets/Scripts/VariableTable.cs
+++ VR-Discounting/Assets/Scripts/VariableTable.cs
@@ -6,6 +6,7 @@ public class VariableTable : MonoBehaviour
 {
 
     public Vector3[] targets = new[] { new Vector3(1, 1, 1) };
+    int x = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -16,14 +17,17 @@ public class VariableTable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        targets = new[] { new Vector3(2, 2, 2) };
         if (Input.GetKeyDown(KeyCode.Alpha0))
         {
-            print("Zero was pressed!");
+            x = 0;
+            Debug.Log("Working?");
         }
         if (Input.GetKeyDown(KeyCode.Alpha1)) 
         {
-            print("One was pressed!");
+            x = 1;
+            Debug.Log("Hi");
         }
-    }
+        targets = new[] { new Vector3(x, x, x) };
+        //Debug.Log(x);
+    } 
 }
