ab87e57ec92706c7e44a095d17b6361014772bb5
Svenskbygderna <87161151+petermgrund@users.noreply.github.com>
2022-01-03 14:36:07 -0600

    created variable table.cs
    Co-Authored-By: Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>

---
+++ VR-Discounting/Assets/Scripts/VariableTable.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableTable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("This is Peter!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
